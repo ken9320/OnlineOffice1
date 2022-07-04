c6bc2bb077b41e44222651867b1568c55cba5bc9
Yin <chenglokyin@gmail.com>
2022-07-04 17:57:33 +0800

    psqy edit

--- localhost(project).session.sql
+++ localhost(project).session.sql
@@ -1,7 +1,15 @@
+CREATE TABLE companys (
+    id SERIAL PRIMARY KEY,
+    companyName VARCHAR(255) NOT NULL,
+    SubscriptionEXpiresday DATE NOT NULL
+)
+
 CREATE TABLE staffs (
     id SERIAL PRIMARY KEY,
     staffid INTEGER NOT NULL,
+    staffPassword VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
+    
     department VARCHAR(255) NOT NULL,
     position VARCHAR(255) NOT NULL,
     entry_date DATE NOT NULL
@@ -9,7 +17,8 @@ CREATE TABLE staffs (
 
 CREATE TABLE schedule (
     id SERIAL PRIMARY KEY,
-    staffid INTEGER NOT NULL,
+    staff_id INTEGER NOT NULL,
+    FOREIGN key (staff_id) REFERENCES staffs (id)
     event TEXT,
     date DATE NOT NULL,
     time TIME NOT NULL,
