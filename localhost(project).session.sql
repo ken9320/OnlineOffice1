06e3346f6bc20b1fdee69f003793a189b8dcef43
Yin <chenglokyin@gmail.com>
2022-07-05 10:50:22 +0800

    sql data upload

--- localhost(project).session.sql
+++ localhost(project).session.sql
@@ -6,6 +6,8 @@ CREATE TABLE companys (
 
 CREATE TABLE staffs (
     id SERIAL PRIMARY KEY,
+    company INTEGER NOT NULL,
+    FOREIGN KEY (company) REFERENCES companys (id),
     staffid INTEGER NOT NULL,
     staffPassword VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
@@ -17,8 +19,8 @@ CREATE TABLE staffs (
 
 CREATE TABLE schedule (
     id SERIAL PRIMARY KEY,
-    staff_id INTEGER NOT NULL,
-    FOREIGN key (staff_id) REFERENCES staffs (id)
+   staff_id INTEGER NOT NULL,
+    FOREIGN key (staff_id) REFERENCES staffs (id),
     event TEXT,
     date DATE NOT NULL,
     time TIME NOT NULL,
@@ -26,3 +28,39 @@ CREATE TABLE schedule (
     updated_at TIMESTAMP NOT NULL
 );
 
+
+
+INSERT INTO public.companys
+(id, companyname, subscriptionexpiresday)
+VALUES(1, '689store', '2022-12-30');
+INSERT INTO public.companys
+(id, companyname, subscriptionexpiresday)
+VALUES(2, 'ok shop', '2022-11-28');
+
+
+INSERT INTO public.staffs
+(id, company, staffid, staffpassword, "name", department, "position", entry_date)
+VALUES(1, 1, 1, '0001', 'Peter', 'CEO', 'CEO', '2000-01-01');
+INSERT INTO public.staffs
+(id, company, staffid, staffpassword, "name", department, "position", entry_date)
+VALUES(2, 1, 2, '0002', 'Tom', 'HR', 'deptHead', '2000-01-02');
+INSERT INTO public.staffs
+(id, company, staffid, staffpassword, "name", department, "position", entry_date)
+VALUES(3, 1, 3, '0003', 'Him', 'logistics ', 'staff', '2000-01-03');
+INSERT INTO public.staffs
+(id, company, staffid, staffpassword, "name", department, "position", entry_date)
+VALUES(4, 1, 4, '0004', 'Ada', 'salesman', 'staff', '2000-01-04');
+INSERT INTO public.staffs
+
+
+(id, company, staffid, staffpassword, "name", department, "position", entry_date)
+VALUES(5, 2, 1, '0001', 'Alex', 'CEO', 'CEO', '2001-01-01');
+INSERT INTO public.staffs
+(id, company, staffid, staffpassword, "name", department, "position", entry_date)
+VALUES(6, 2, 2, '0002', 'Angel', 'HR', 'deptHead', '2001-01-02');
+INSERT INTO public.staffs
+(id, company, staffid, staffpassword, "name", department, "position", entry_date)
+VALUES(7, 2, 3, '0003', 'Angela', 'logistics ', 'staff', '2001-01-03');
+INSERT INTO public.staffs
+(id, company, staffid, staffpassword, "name", department, "position", entry_date)
+VALUES(8, 2, 4, '0004', 'Him', 'salesman	', 'staff', '2001-01-04');
