8020e32b26fb100ea76754ede847057da91964fb
ken9320 <kenlee241@gmail.com>
2022-07-13 15:15:49 +0800

    updatedPage

--- etherPad.ts
+++
@@ -1,8 +0,0 @@
-function runetherPad(): void {
-	const shell = require('shelljs')
-	shell.exec(
-		'C:\\Users\\user\\Desktop\\myTeckyWork\\git_Project\\OnlineOffice\\etherpad-lite\\src\\bin\\run.sh'
-	)
-}
-
-runetherPad()
